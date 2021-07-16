ce33fca2ca9cc0f20eecf9312dedbe09fb5c8602
Tony Chen <tchen@cio46jgh6mac.myfiosgateway.com>
2021-07-16 06:19:08 -0400

    first commit

---
+++ install_apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt update && apt upgrade -y 
+declare -a packages=("p7zip" "audacity" "firefox" "gimp" "handbrake" "inkscape" "krita" "libreoffice" "tux-paint" "vlc" "chromium-browser")
+for package in ${packages[@]}; do
+    echo "******************" 
+    echo $package
+    echo "******************"
+    apt install  $package -y
+done
+wget https://zoom.us/client/latest/zoom_amd64.deb -O /tmp/zoom_amd64.deb 
+echo "******************" 
+echo zoom
+echo "******************"
+apt install --fix-broken /tmp/zoom_amd64.deb -y
+
