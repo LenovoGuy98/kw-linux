3f325f1588286162f7ce27aa002bd08baf240e5a
velogear <lenovoguy98@gmail.com>
2025-08-28 15:11:58 -0400

    Updated Scripts both add and update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # update linux and install apps script
 
-- install_apps.sh  will install p7zip audacity firefox gimp handbrake inkscape krita libreoffice tux-paint vlc zoom and google-chrome
+- install_apps.sh  will install  audacity firefox libreoffice zoom google-chrome kw-starup
 
--- install_apps.sh
+++ install_apps.sh
@@ -17,3 +17,11 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -
 apt install /tmp/google-chrome-stable_current_amd64.deb -y
 wget https://novabench.com/files/novabench-x64.deb  -O /tmp/novabench-x64.deb
 apt install /tmp/novabench-x64.deb -y
+
+#KindWorks Stuff
+mkdir ~/kindworks
+cd ~/kindworks
+wget https://github.com/LenovoGuy98/kw-startup-go/raw/refs/heads/master/kw-startup
+wget https://github.com/LenovoGuy98/kw-startup-go/raw/refs/heads/master/kindworks.png
+wget https://github.com/LenovoGuy98/kw-startup-go/raw/refs/heads/master/Your-Linux-system.pdf
+chmod 755 kw-startup
